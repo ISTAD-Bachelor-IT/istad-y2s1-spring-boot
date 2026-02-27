2bc42eeacfe7d427ace0dbc38947395a3edd309c
oudom <oudomphoem@gmail.com>
2026-02-27 20:33:49 +0700

    new web mvc project

---
+++ spring-boot-webmvc/src/main/java/dev/oudom/webmvc/SpringBootWebmvcApplication.java
@@ -0,0 +1,17 @@
+package dev.oudom.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Structure project
+// 1. Structure by layer
+// 2. Structure by features
+
+@SpringBootApplication
+public class SpringBootWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebmvcApplication.class, args);
+	}
+
+}
