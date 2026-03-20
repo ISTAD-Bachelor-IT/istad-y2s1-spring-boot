3161d2685fbf84a94bca2557daa8755c299c0ed8
oudom <oudomphoem@gmail.com>
2026-03-20 20:28:44 +0700

    handle validation error

--- spring-boot-webmvc/src/main/java/dev/oudom/webmvc/exception/ErrorResponse.java
+++ spring-boot-webmvc/src/main/java/dev/oudom/webmvc/exception/ErrorResponse.java
@@ -3,12 +3,14 @@ package dev.oudom.webmvc.exception;
 import lombok.Builder;
 
 import java.time.Instant;
+import java.util.List;
 
 @Builder
 public record ErrorResponse(
         String status,
         Integer code,
         String message,
-        Instant timestamp
+        Instant timestamp,
+        List<FieldResponse> errorDetails
 ) {
 }
