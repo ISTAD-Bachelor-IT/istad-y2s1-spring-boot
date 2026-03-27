e0266067d803b209eedcfe5787ef0e8c187fd593
oudom <oudomphoem@gmail.com>
2026-03-27 18:47:44 +0700

    init categories

--- spring-boot-webmvc/src/main/java/dev/oudom/webmvc/domain/Category.java
+++ spring-boot-webmvc/src/main/java/dev/oudom/webmvc/domain/Category.java
@@ -1,6 +1,7 @@
 package dev.oudom.webmvc.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 // Make Entity
 @Entity
 @Table(name = "categories")
